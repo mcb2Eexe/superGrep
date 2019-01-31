f1c772823ebb7e2eaa466346a4a603c2d18a250c
mcbma <47228764+mcbma@users.noreply.github.com>
2019-01-31 21:55:41 +0000

    Add files via upload

---
+++ preprocessing.py
@@ -0,0 +1,30 @@
+from multiprocessing import Pool, Queue
+import os
+import sys
+import glob
+
+def process(queue):
+    while not queue.empty():
+        filename = queue.get()
+        print(filename)
+        searchfile = open(filename, 'r')
+        for line in searchfile:
+            if searchString in line:
+                print(line)
+                with open("results.txt", "a+") as result:
+                    result.write(line)
+        searchfile.close()
+
+#def Queue():
+
+if __name__ == '__main__':
+    searchString = input('Enter string to search: ')
+    queue = Queue()
+    for f in os.listdir('.'):
+        if f.endswith('.txt'):
+            print(f)
+            queue.put(f)
+    pool = Pool(None, process, (queue,))
+
+    pool.close()
+    pool.join()
