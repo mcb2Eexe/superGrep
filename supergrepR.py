8999bc093d9b37316b5e68555ff1d71fa21593f6
mcbma <47228764+mcbma@users.noreply.github.com>
2019-02-03 12:53:54 +0000

    Add files via upload

---
+++ supergrepR.py
@@ -0,0 +1,44 @@
+# - supergrepR V1
+# - Written by: McB
+# - Date written: 01 Feb 2019
+# - Last modified: 01 Feb 2019
+# - Designed to search multiple large text files using regex
+# - Uses multiprocessing to search multiple files concurrently
+# - To do's = add counters for total files, files remaining, number of matches
+# - Add windows compatibility(currently linux only)
+
+from multiprocessing import Pool, Queue
+import os
+import multiprocessing
+import re
+
+
+def process(queue):
+    while not queue.empty():
+        dir1 = 'results'
+        if not os.path.exists(dir1):
+            os.makedirs(dir1)
+        filename = queue.get()
+        print(multiprocessing.current_process())
+        print(filename)
+        print('sS= ', searchString)
+        regex = re.compile('{}'.format(searchString))
+        print(regex)
+        searchfile = open(filename, 'r')
+        for line in searchfile:
+            if regex.search(line) is not None:
+                with open("results/results.txt", "a+") as result:
+                    result.write(line)
+        searchfile.close()
+
+
+if __name__ == '__main__':
+    print('Run in same dir as text files to search')
+    searchString = input('Input regex search: ')
+    queue = Queue()
+    for f in os.listdir('.'):
+        if f.endswith('.txt'):
+            queue.put(f)
+    pool = Pool(None, process, (queue,))
+    pool.close()
+    pool.join()
